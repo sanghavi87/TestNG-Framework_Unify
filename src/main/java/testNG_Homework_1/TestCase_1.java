e38bd2fac73bfa6894116e0797cbfd798c710631
Abhishek Sanghavi <abhi.sanghavi@ggmail.com>
2023-04-26 12:17:53 +0100

    TestNG

---
+++ src/main/java/testNG_Homework_1/TestCase_1.java
@@ -0,0 +1,45 @@
+package testNG_Homework_1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestCase_1 {
+    WebDriver driver;
+
+
+@Test(priority= 1)
+        void openBrowser()
+{
+    System.out.println("Open Browser");
+    WebDriverManager.chromedriver().setup();
+    driver=new ChromeDriver();
+    driver.get("https://demo.nopcommerce.com/");
+    driver.manage().window().maximize();
+}
+
+@Test(priority= 2)
+    void login()
+
+{
+    System.out.println("Run Login Functionality ");
+    driver.findElement(By.xpath("/html/body/div[6]/div[1]/div[1]/div[2]/div[1]/ul/li[2]/a")).click();
+    driver.findElement(By.id("Email")).sendKeys("rohan@gmail.com");
+    driver.findElement(By.className("password")).sendKeys("rohan123");
+    driver.findElement(By.xpath("//button[normalize-space()='Log in']")).click();
+
+}
+
+
+@Test(priority = 3)
+    void closeBrowser()
+{
+    System.out.println("Close Browser");
+    driver.close();
+}
+
+
+
+}
